f8f469b23646a850cb4cb5253f62ff3a3498934d
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-03-20 12:20:52 +0100

    working towards being able to display current date, month etc.

--- Calendar/MainWindow.xaml.cs
+++ Calendar/MainWindow.xaml.cs
@@ -1,24 +1,84 @@
-﻿using System.Text;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calendar
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private DateTime _currentDate;
+        private string _currentDayOfWeek;
+        private int _currentDay;
+        private int _currentMonth;
+        private int _currentYear;
+
+        public DateTime CurrentDate
+        {
+            get { return _currentDate; }
+            set
+            {
+                _currentDate = value;
+                OnPropertyChanged();
+            }
+        }
+        public string CurrentDayOfWeek
+        {
+            get { return _currentDayOfWeek; }
+            set
+            {
+                _currentDayOfWeek = value;
+                OnPropertyChanged();
+            }
+        }
+        public int CurrentDay
+        {
+            get { return _currentDay; }
+            set
+            {
+                _currentDay = value;
+                OnPropertyChanged();
+            }
+        }
+        public int CurrentMonth
+        {
+            get { return _currentMonth; }
+            set
+            {
+                _currentMonth = value;
+                OnPropertyChanged();
+            }
+        }
+        public int CurrentYear
+        {
+            get { return _currentYear; }
+            set
+            {
+                _currentYear = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
+            CurrentDate = DateTime.Now;
+            CurrentDayOfWeek = CurrentDate.DayOfWeek.ToString();
+            CurrentDay = CurrentDate.Day;
+            CurrentMonth = CurrentDate.Month;
+            CurrentYear = CurrentDate.Year;
+
             InitializeComponent();
+            DataContext = this; // Set the DataContext to this MainWindow instance
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
