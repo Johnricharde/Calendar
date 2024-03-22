cfa23624fcfa7ecbdff9b05ae29227d9935de734
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-03-22 13:19:51 +0100

    Cleaned up code

--- Calendar/MainWindow.xaml.cs
+++ Calendar/MainWindow.xaml.cs
@@ -15,10 +15,53 @@ namespace Calendar
         private int _currentDay;
         private int _currentMonth;
         private int _currentYear;
+        public DateTime CurrentDate
+        {
+            get { return _currentDate; }
+            set { _currentDate = value; OnPropertyChanged(); }
+        }
+        public string CurrentDayOfWeek
+        {
+            get { return _currentDayOfWeek; }
+            set { _currentDayOfWeek = value; OnPropertyChanged(); }
+        }
+        public int CurrentDay
+        {
+            get { return _currentDay; }
+            set { _currentDay = value; OnPropertyChanged(); }
+        }
+        public int CurrentMonth
+        {
+            get { return _currentMonth; }
+            set { _currentMonth = value; OnPropertyChanged(); }
+        }
+        public int CurrentYear
+        {
+            get { return _currentYear; }
+            set { _currentYear = value; OnPropertyChanged(); }
+        }
+        public MainWindow()
+        {
+            CurrentDate = DateTime.Now;
+            CurrentDayOfWeek = CurrentDate.DayOfWeek.ToString();
+            CurrentDay = CurrentDate.Day;
+            CurrentMonth = CurrentDate.Month;
+            CurrentYear = CurrentDate.Year;
+
+            InitializeComponent();
+            DataContext = this;
+            PopulateCalendarGrid();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
 
         public string FormattedDate => $"{CurrentDate.ToString("MMMM yyyy")}";
-
-
 
 
         private void PopulateCalendarGrid()
@@ -79,79 +122,6 @@ namespace Calendar
             Grid.SetRow(border, row);
             Grid.SetColumn(border, column);
             calendarGrid.Children.Add(border);
-        }
-
-
-
-
-
-
-
-
-        public DateTime CurrentDate
-        {
-            get { return _currentDate; }
-            set
-            {
-                _currentDate = value;
-                OnPropertyChanged();
-            }
-        }
-        public string CurrentDayOfWeek
-        {
-            get { return _currentDayOfWeek; }
-            set
-            {
-                _currentDayOfWeek = value;
-                OnPropertyChanged();
-            }
-        }
-        public int CurrentDay
-        {
-            get { return _currentDay; }
-            set
-            {
-                _currentDay = value;
-                OnPropertyChanged();
-            }
-        }
-        public int CurrentMonth
-        {
-            get { return _currentMonth; }
-            set
-            {
-                _currentMonth = value;
-                OnPropertyChanged();
-            }
-        }
-        public int CurrentYear
-        {
-            get { return _currentYear; }
-            set
-            {
-                _currentYear = value;
-                OnPropertyChanged();
-            }
-        }
-
-        public MainWindow()
-        {
-            CurrentDate = DateTime.Now;
-            CurrentDayOfWeek = CurrentDate.DayOfWeek.ToString();
-            CurrentDay = CurrentDate.Day;
-            CurrentMonth = CurrentDate.Month;
-            CurrentYear = CurrentDate.Year;
-
-            InitializeComponent();
-            DataContext = this;
-            PopulateCalendarGrid();
-        }
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
